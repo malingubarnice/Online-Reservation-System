e2ac5f9546167dbc9db7a9562318181308348595
malingubarnice <120421629+malingubarnice@users.noreply.github.com>
2025-02-01 18:52:30 +0300

    adding my first mpesa file

--- mpesa_integration/mpesa_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("CONSUMER_KEY", "YOUR_CONSUMER_KEY");
-define("CONSUMER_SECRET", "YOUR_CONSUMER_SECRET");
-define("BUSINESS_SHORTCODE", "174379"); // Your Paybill or Till Number
-define("PASSKEY", "YOUR_MPESA_PASSKEY");
-define("CALLBACK_URL", "https://yourwebsite.com/backend/callback.php");
-?>
--- mpesa_integration/success.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "<h2>Payment Successful!</h2>";
-?>
