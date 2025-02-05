a57bbaaedcaa1b42bf4346842860eb01ebb9965e
malingubarnice <120421629+malingubarnice@users.noreply.github.com>
2025-02-05 13:36:52 +0300

    adding new data

---
+++ .history/query_20250205132040.php
@@ -0,0 +1,23 @@
+<?php
+//INCLUDE ACCESS TOKEN FILE 
+include 'accessToken.php';
+date_default_timezone_set('Africa/Nairobi');
+$query_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query';
+$BusinessShortCode = '174379';
+$Timestamp = date('YmdHis');
+$passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
+// ENCRIPT  DATA TO GET PASSWORD
+$Password = base64_encode($BusinessShortCode . $passkey . $Timestamp);
+//THIS IS THE UNIQUE ID THAT WAS GENERATED WHEN STK REQUEST INITIATED SUCCESSFULLY
+$CheckoutRequestID = 'ws_CO_03072023054410314768168060';
+$queryheader = ['Content-Type:application/json', 'Authorization:Bearer ' . $access_token];
+# initiating the transaction
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $query_url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $queryheader); //setting custom header
+$curl_post_data = array(
+  'BusinessShortCode' => $BusinessShortCode,
+  'Password' => $Password,
+  'Timestamp' => $Timestamp,
+  'CheckoutRequestID' => $CheckoutRequestID
+);
